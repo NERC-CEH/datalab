3ec351b4e08bc22254842e688eeb855e35bce946
Joshua Foster <joshua.foster@tessella.com>
2017-05-02 15:50:56 +0100

    Fix: Remove unneeded prop in NavMenu

--- code/datalab-app/src/components/app/NavMenu.js
+++ code/datalab-app/src/components/app/NavMenu.js
@@ -2,7 +2,7 @@ import React from 'react';
 import MenuItem from 'material-ui/MenuItem';
 import { Link } from 'react-router-dom';
 
-const NavMenu = ({ hideMenu, history }) => (
+const NavMenu = ({ hideMenu }) => (
     <div>
       <MenuItem onTouchTap={hideMenu}><Link to='/'>Home Page</Link></MenuItem>
       <MenuItem onTouchTap={hideMenu}><Link to='/example'>Example Page</Link></MenuItem>
