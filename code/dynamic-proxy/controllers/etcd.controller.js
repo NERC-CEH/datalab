68be85c02ff244803d0eb4f1072153201e5baca9
Gary Lloyd <gareth.lloyd@tessella.com>
2017-05-04 08:29:07 +0100

    Refactor: Tidy arrow functions in controller

--- code/dynamic-proxy/controllers/etcd.controller.js
+++ code/dynamic-proxy/controllers/etcd.controller.js
@@ -2,7 +2,11 @@ import etcdService from '../services/etcd.service.js';
 
 function listRoutes(req, res, next) {
   etcdService.getRoutes()
-    .then((routes) => res.send(routes));
+    .then((routes) => res.send(routes))
+    .catch((error) => {
+      res.status(500);
+      res.send({ message: `Error loading routes`});
+    });
 }
 
 function addRoute(req, res, next) {
@@ -19,9 +23,7 @@ function addRoute(req, res, next) {
 
 function deleteRoute(req, res, next) {
   etcdService.deleteRoute(req.body.source)
-    .then((response) => {
-      res.send(response);
-    })
+    .then((response) => res.send(response))
     .catch((error) => {
       res.status(500);
       res.send({ message: `Error deleting route ${req.body.source}: ${error}`});
@@ -30,9 +32,7 @@ function deleteRoute(req, res, next) {
 
 function deleteAllRoutes(req, res, next) {
   etcdService.deleteAllRoutes()
-    .then((response) => {
-      res.send(response);
-    })
+    .then((response) => res.send(response))
     .catch((error) => {
       res.status(500);
       res.send({ message: `Error delecting all routes: ${error}`})
