a7296e6d546f2bad48b839002db533ec0aa214d5
Joshua Foster <joshua.foster@tessella.com>
2017-05-09 10:54:21 +0100

    Chore: Move react-app to app sub-directory

--- code/datalab-app/src/index.js
+++ code/datalab-app/src/index.js
@@ -1,21 +1,2 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import injectTapEventPlugin from 'react-tap-event-plugin';
-import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
-import configureStore from './store/configureStore';
-import Router from './router';
-
-const store = configureStore();
-
-// Needed for onTouchTap -- will be fixed in future react release
-injectTapEventPlugin();
-
-ReactDOM.render(
-  <MuiThemeProvider>
-    <Provider store={store}>
-      <Router />
-    </Provider>
-  </MuiThemeProvider>,
-  document.getElementById('root'),
-);
+// For compatibility with react-scripts/create-react-app the index of src must load the app
+import app from './app/app'; // eslint-disable-line no-unused-vars
