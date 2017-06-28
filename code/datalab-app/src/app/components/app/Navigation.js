9eb7d5b6146abbf8071442a3f8105d7583cbc8d4
Joshua Foster <joshua.foster@tessella.com>
2017-06-28 23:45:09 +0100

    Feature: Add login-logout button to navbar

--- code/datalab-app/src/app/components/app/Navigation.js
+++ code/datalab-app/src/app/components/app/Navigation.js
@@ -1,35 +1,69 @@
-import React from 'react';
-import PropTypes from 'prop-types';
+import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 import AppBar from 'material-ui/AppBar';
 import Drawer from 'material-ui/Drawer';
+import RaisedButton from 'material-ui/RaisedButton';
 import NavMenu from './NavMenu';
 import menuActions from '../../actions/menuActions';
+import authActions from '../../actions/authActions';
+import { login, logout, isAuthenticated } from '../../auth/auth';
 
-const Navigation = ({ isMenuOpen, actions: { showMenu, hideMenu } }) => (
-  <div>
-    <AppBar title="Data Labs" onLeftIconButtonTouchTap={showMenu} />
-    <Drawer open={isMenuOpen} docked={false} onRequestChange={hideMenu}>
-      <NavMenu hideMenu={hideMenu} />
-    </Drawer>
-  </div>
-);
+class Navigation extends Component {
+  constructor(props, context) {
+    super(props, context);
+    this.userLoggedIn = this.userLoggedIn.bind(this);
+    this.userLoginLogout = this.userLoginLogout.bind(this);
+  }
 
-Navigation.propTypes = {
-  isMenuOpen: PropTypes.bool.isRequired,
-  actions: PropTypes.shape({
-    showMenu: PropTypes.func.isRequired,
-    hideMenu: PropTypes.func.isRequired,
-  }).isRequired,
-};
+  userLoggedIn() {
+    return isAuthenticated(this.props.user);
+  }
 
-function mapStateToProps(state) {
-  return state.menu;
+  userLoginLogout() {
+    if (this.userLoggedIn()) {
+      logout().then(() => this.props.actions.userLogsOut());
+    } else {
+      // Not promise as user will be redirected away from page.
+      login();
+    }
+  }
+
+  render() {
+    return (
+      <div>
+        <AppBar
+          title="Data Labs"
+          onLeftIconButtonTouchTap={this.props.actions.showMenu}
+          onRightIconButtonTouchTap={this.userLoginLogout}
+          iconElementRight={
+            <RaisedButton
+              label={this.userLoggedIn() ? 'Logout' : 'Login'}
+              secondary
+            />}
+        />
+        <Drawer open={this.props.menu.isMenuOpen} docked={false} onRequestChange={this.props.actions.hideMenu}>
+          <NavMenu hideMenu={this.props.actions.hideMenu} />
+        </Drawer>
+      </div>
+    );
+  }
+}
+
+function mapStateToProps({ menu, authentication: { user } }) {
+  return {
+    menu,
+    user,
+  };
 }
 
 function mapDispatchToProps(dispatch) {
-  return { actions: bindActionCreators({ ...menuActions }, dispatch) };
+  return {
+    actions: bindActionCreators({
+      ...authActions,
+      ...menuActions,
+    }, dispatch),
+  };
 }
 
 export default connect(mapStateToProps, mapDispatchToProps)(Navigation);
