65b81dd68eb210a7740af3dae107cb1a5dd2e147
Joshua Foster <joshua.foster@tessella.com>
2017-06-29 08:23:53 +0100

    Feature: Add RequireAuth to mask private pages

---
+++ code/datalab-app/src/app/pages/PrivatePage.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const PrivatePage = () => (
+  <div>
+    <h1>Private Content</h1>
+    <p>This page is masked from user that are not signed in.</p>
+  </div>
+);
+
+export default PrivatePage;
--- code/datalab-app/src/app/router.js
+++ code/datalab-app/src/app/router.js
@@ -2,12 +2,14 @@ import React from 'react';
 import { ConnectedRouter } from 'react-router-redux';
 import { Route, Switch } from 'react-router-dom';
 import browserHistory from './store/browserHistory';
+import RequireAuth from './components/app/RequireAuth';
 import App from './pages/App';
 import HomePage from './pages/HomePage';
 import ExamplePage from './pages/ExamplePage';
 import ApiExamplePage from './pages/ApiExamplePage';
 import NotFoundPage from './pages/NotFoundPage';
 import CallbackPage from './pages/CallbackPage';
+import PrivatePage from './pages/PrivatePage';
 
 const router = () => (
   <ConnectedRouter history={browserHistory} >
@@ -18,6 +20,7 @@ const router = () => (
         <Route exact path="/callback" component={CallbackPage} />
         <Route exact path="/example" component={ExamplePage} />
         <Route exact path="/apiExample" component={ApiExamplePage} />
+        <RequireAuth exact path="/private" component={PrivatePage} />
         <Route component={NotFoundPage} />
       </Switch>
       </App>
