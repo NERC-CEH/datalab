976358ae743c42beae0a3fccb0607105217bc673
Joshua Foster <joshua.foster@tessella.com>
2017-07-04 18:13:40 +0100

    Feature: Create SemanticUI NavBar

--- code/datalab-app/src/app/app.js
+++ code/datalab-app/src/app/app.js
@@ -1,21 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import injectTapEventPlugin from 'react-tap-event-plugin';
-import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import { Container } from 'semantic-ui-react';
 import configureStore from './store/configureStore';
 import Router from './router';
 
 const store = configureStore();
 
-// Needed for onTouchTap -- will be fixed in future react release
-injectTapEventPlugin();
-
 ReactDOM.render(
-  <MuiThemeProvider>
+  <Container>
     <Provider store={store}>
       <Router />
     </Provider>
-  </MuiThemeProvider>,
+  </Container>,
   document.getElementById('root'),
 );
