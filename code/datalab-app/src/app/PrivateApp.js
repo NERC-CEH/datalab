9c7c52289d7e7eede4b4caf025e2f407dd654d0e
Joshua Foster <joshua.foster@tessella.com>
2017-07-11 12:41:49 +0100

    Chore: Remove example code

--- code/datalab-app/src/app/PrivateApp.js
+++ code/datalab-app/src/app/PrivateApp.js
@@ -4,13 +4,11 @@ import Navigation from './components/app/Navigation';
 import NotFoundPage from './pages/NotFoundPage';
 import Footer from './components/app/Footer';
 import HomePage from './pages/HomePage';
-import PrivatePage from './pages/PrivatePage';
 
 const PrivateApp = () => (
   <Navigation>
     <Switch>
       <Route exact path="/" component={HomePage} />
-      <Route exact path="/private" component={PrivatePage} />
       <Route component={NotFoundPage} />
     </Switch>
     <Route component={Footer} />
