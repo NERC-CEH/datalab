b60983f4e77a63e1bd32c2fdfea0b213a2e1a50b
Joshua Foster <joshua.foster@tessella.com>
2017-07-13 18:04:53 +0100

    Feature: Create data storage table

---
+++ code/datalab-app/src/app/components/datastorage/DataStorageTableContainer.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import dataStorageActions from '../../actions/dataStorageActions';
+import DataStorageTable from './DataStorageTable';
+
+class DataStorageTableContainer extends Component {
+  componentWillMount() {
+    this.props.actions.loadDataStorage();
+  }
+
+  render() {
+    return (
+      <DataStorageTable dataStorage={this.props.dataStorage.value} />
+    );
+  }
+}
+
+function mapStateToProps({ dataStorage }) {
+  return { dataStorage };
+}
+
+function mapDispatchToProps(dispatch) {
+  return {
+    actions: bindActionCreators({
+      ...dataStorageActions,
+    }, dispatch),
+  };
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(DataStorageTableContainer);
