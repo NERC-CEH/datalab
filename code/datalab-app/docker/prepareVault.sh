527b5265714fda1283ba0479f21805ecd30a00f8
Gary Lloyd <gareth.lloyd@tessella.com>
2017-07-26 18:43:28 +0100

    Feature: Extend local docker config with minio container

--- code/datalab-app/docker/prepareVault.sh
+++ code/datalab-app/docker/prepareVault.sh
@@ -17,9 +17,9 @@ curl -X POST \
   -d '{"policies":"datalab-policy", "bound_cidr_list": "172.18.0.1/32", "bind_secret_id":"false"}' \
   $VAULT_ADDR/v1/auth/approle/role/datalab
 
-export VAULT_APP_ROLE=$(curl -s -X GET -H "X-Vault-Token:$VAULT_TOKEN" http://127.0.0.1:8200/v1/auth/approle/role/datalab/role-id | jq .data.role_id)
+VAULT_APP_ROLE=$(curl -s -X GET -H "X-Vault-Token:$VAULT_TOKEN" http://127.0.0.1:8200/v1/auth/approle/role/datalab/role-id | jq .data.role_id)
 
-echo $VAULT_APP_ROLE
+echo export VAULT_APP_ROLE=${VAULT_APP_ROLE}
 
 # Add default keys
-curl -X POST -H "X-Vault-Token:$VAULT_TOKEN" -d '{"access_key":"accesskey1","secret_key":"secretkey1"}' http://127.0.0.1:8200/v1/secret/datalab/files/files1
+curl -X POST -H "X-Vault-Token:$VAULT_TOKEN" -d '{"access_key":"VXL8C2ITGFBVMHKV2EV6","secret_key":"iUOhbVYNRxJS2X+5Ukpvsk01TsVWxbdERaNGf5kI"}' http://127.0.0.1:8200/v1/secret/datalab/storage/disk-1
