593069bb0b3b4cebe309937f613e6592ad3c9d6a
Gary Lloyd <gareth.lloyd@tessella.com>
2017-07-28 08:16:51 +0100

    Feature: Add Minio API service

---
+++ code/datalab-app/src/app/api/minioService.spec.js
@@ -0,0 +1,64 @@
+import crossStorage from 'cross-storage';
+import minioService from './minioService';
+
+const store = [];
+let connectUrl;
+const mockSuccessfulCrossStorage = class CrossStorageClient {
+  constructor(url) {
+    connectUrl = url;
+  }
+
+  onConnect() {
+    return Promise.resolve();
+  }
+
+  set(key, value) {
+    store.push({ key, value });
+    return Promise.resolve();
+  }
+};
+
+const mockFailedCrossStorage = class CrossStorageClient {
+  onConnect() {
+    return Promise.reject('error');
+  }
+};
+
+describe('minioService', () => {
+  beforeEach(() => {
+    store.length = 0;
+    connectUrl = undefined;
+  });
+
+  describe('openStorage', () => {
+    it('should connect to cross storage set token and open tab', () => {
+      const url = 'http://localhost:3000/minio';
+      const token = 'tokenValue';
+
+      crossStorage.CrossStorageClient = mockSuccessfulCrossStorage;
+      global.open = jest.fn();
+
+      return minioService.openStorage(url, token)
+        .then(() => {
+          expect(connectUrl).toEqual('http://localhost:3000/connect');
+          expect(store).toEqual([{ key: 'token', value: 'tokenValue' }]);
+          expect(global.open).toBeCalledWith(url);
+        });
+    });
+
+    it('should return a rejected promise if connect fails', () => {
+      const url = 'http://localhost:3000/minio';
+      const token = 'tokenValue';
+
+      crossStorage.CrossStorageClient = mockFailedCrossStorage;
+
+      return minioService.openStorage(url, token)
+        .then(() => {
+          expect(true).toBe(false);
+        })
+        .catch((error) => {
+          expect(error).toBe('error');
+        });
+    });
+  });
+});
