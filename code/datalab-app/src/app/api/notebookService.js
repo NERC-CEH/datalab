1bf8b40ee0cf5a8ee2e4125aa366fe826f22fa10
Joshua Foster <joshua.foster@tessella.com>
2017-08-02 16:58:19 +0100

    Chore: Update api request and response handling

--- code/datalab-app/src/app/api/notebookService.js
+++ code/datalab-app/src/app/api/notebookService.js
@@ -5,7 +5,7 @@ import apiBase from './apiBase';
 const apiURL = `${apiBase}/api`;
 
 function loadNotebooks() {
-  return request.post(apiURL, { query: '{ notebooks { name url cookie } }' })
+  return request.post(apiURL, { query: '{ notebooks { name url token } }' })
     .then(res => get(res, 'data.data.notebooks'));
 }
 
--- code/datalab-app/src/app/components/notebooks/NotebookButton.js
+++ code/datalab-app/src/app/components/notebooks/NotebookButton.js
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types';
 import { Button } from 'semantic-ui-react';
 
 const NotebookButton = ({ notebook, openNotebookAction }) => (
-  <Button primary onClick={() => openNotebookAction(notebook.url, notebook.cookie)} >
+  <Button primary onClick={() => openNotebookAction(notebook.url, notebook.token)} >
     {notebook.name}
   </Button>
 );
@@ -12,7 +12,7 @@ NotebookButton.propTypes = {
   notebook: PropTypes.shape({
     name: PropTypes.string,
     url: PropTypes.string,
-    cookie: PropTypes.string,
+    token: PropTypes.string,
   }),
   openNotebookAction: PropTypes.func.isRequired,
 };
