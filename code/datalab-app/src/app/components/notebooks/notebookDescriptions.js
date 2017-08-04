8831e49f0855187c74edfef8dad0f6ae287a84f2
Joshua Foster <joshua.foster@tessella.com>
2017-08-04 15:10:50 +0100

    Fix: Correct typo in directory name

--- code/datalab-app/src/app/components/notebooks/notebookDescriptions.js
+++ code/datalab-app/src/app/components/notebooks/notebookDescriptions.js
@@ -1,6 +1,6 @@
 import { JUPYTER, ZEPPELIN } from '../../../shared/notebookTypes';
-import jupyerLogo from '../../../assests/images/jupyter-logo.svg';
-import zeppelinLogo from '../../../assests/images/zeppelin-logo.svg';
+import jupyerLogo from '../../../assets/images/jupyter-logo.svg';
+import zeppelinLogo from '../../../assets/images/zeppelin-logo.svg';
 
 const jupyterDescription = 'Web application that allows you to create and share documents that contain live code, equations, visualizations and explanatory text.';
 const zeppelinDescription = 'Web-based notebook that enables data-driven, interactive data analytics and collaborative documents with SQL, Scala and more.';
