9e78c0c6dd11a1502c7c3c44c058eb34fac0735f
Joshua Foster <joshua.foster@tessella.com>
2017-08-04 15:40:00 +0100

    Tests: Update unit tests

---
+++ code/datalab-app/src/app/components/notebooks/NotebookCards.spec.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import NotebookCards from './NotebookCards';
+
+describe('NotebookCards', () => {
+  function shallowRender(props) {
+    return shallow(<NotebookCards {...props} />);
+  }
+
+  const generateProps = () => ({
+    notebooks: [
+      { displayName: 'name1', redirectUrl: 'url1', type: 'type1' },
+      { displayName: 'name2', redirectUrl: 'url2', type: 'type2' },
+      { displayName: 'name3', redirectUrl: 'url3', type: 'type3' },
+    ],
+    openNotebook: () => {},
+  });
+
+  it('creates correct snapshot for an array of notebooks', () => {
+    // Arrange
+    const props = generateProps();
+
+    // Act
+    const output = shallowRender(props);
+
+    // Assert
+    expect(output).toMatchSnapshot();
+  });
+
+  it('creates correct snapshot for an empty array', () => {
+    // Arrange
+    const props = { ...generateProps(), notebooks: [] };
+
+    // Act
+    const output = shallowRender(props);
+
+    // Assert
+    expect(output).toMatchSnapshot();
+  });
+});
