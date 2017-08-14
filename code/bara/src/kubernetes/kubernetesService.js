7d2697958cf39fafb30abc0ff8aa392ebf064c19
Gary Lloyd <gareth.lloyd@tessella.com>
2017-08-14 16:34:09 +0100

    Feature: Improving logging and execution tidy up

--- code/bara/src/kubernetes/kubernetesService.js
+++ code/bara/src/kubernetes/kubernetesService.js
@@ -1,4 +1,5 @@
 import fs from 'fs-extra-promise';
+import del from 'del';
 import chalk from 'chalk';
 import path from 'path';
 import { render } from 'mustache';
@@ -17,20 +18,40 @@ function deployManifest(templatePath, configPath) {
   fs.readFileAsync(templatePath)
     .then(data => data.toString())
     .then(template => render(template, properties))
-    .then(renderedTemplate => fs.writeFileAsync(targetFilename, renderedTemplate))
-    .then(() => execAsync(`kubectl apply -f ${targetFilename}`))
+    .then(writeRenderedTemplate(targetFilename))
+    .then(executeKubectl(targetFilename))
     .then(processResponse)
+    .then(tidyUp)
     .catch(console.error);
 }
 
 function prepareWorkingSpace() {
-  if (!fs.existsSync(executionDir)) {
-    fs.mkdirSync(executionDir);
+  if (fs.existsSync(executionDir)) {
+    console.log(chalk.yellow(`Deleting existing ${executionDir} directory`));
+    del.sync(executionDir);
   }
+  console.log(chalk.blue(`Creating ${executionDir} directory`));
+  fs.mkdirSync(executionDir);
 }
 
 function getTargetFileName(sourcePath) {
-  return `${executionDir}/${path.basename(sourcePath)}`;
+  let filename = path.basename(sourcePath);
+  filename = filename.replace('.template', '');
+  return `${executionDir}/${filename}`;
+}
+
+function writeRenderedTemplate(targetFilename) {
+  return (renderedContent) => {
+    console.log(chalk.blue(`Writing rendered template to : ${targetFilename}`));
+    fs.writeFileAsync(targetFilename, renderedContent);
+  };
+}
+
+function executeKubectl(targetFilename) {
+  return () => {
+    console.log(chalk.blue(`Executing kubectl for template : ${targetFilename}`));
+    return execAsync(`kubectl apply -f ${targetFilename}`);
+  };
 }
 
 function processResponse(response) {
@@ -38,6 +59,13 @@ function processResponse(response) {
     console.log(chalk.green(response.stdout));
   } else {
     console.log(chalk.red(response.stderr));
+  }
+}
+
+function tidyUp() {
+  if (fs.existsSync(executionDir)) {
+    console.log(chalk.blue(`Tidy up ${executionDir} directory`));
+    del.sync(executionDir);
   }
 }
 
