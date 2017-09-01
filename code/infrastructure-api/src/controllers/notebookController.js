b35d01e7ee0deab965f52c433d0b1279cf8851f8
Gary Lloyd <gareth.lloyd@tessella.com>
2017-09-01 18:45:32 +0100

    Feature: Improve error handling

--- code/infrastructure-api/src/controllers/notebookController.js
+++ code/infrastructure-api/src/controllers/notebookController.js
@@ -3,29 +3,33 @@ import { check, validationResult } from 'express-validator/check';
 import { matchedData } from 'express-validator/filter';
 import notebookManager from '../notebooks/notebookManager';
 
-function createNotebook(req, res) {
+function createNotebook(request, response) {
   // Parse request for errors
-  const errors = validationResult(req);
+  const errors = validationResult(request);
   if (!errors.isEmpty()) {
-    logger.error(`Invalid notebook creation request: ${req.body}`);
-    return res.status(400).json({ errors: errors.mapped() });
+    logger.error(`Invalid notebook creation request: ${request.body}`);
+    return response.status(400).json({ errors: errors.mapped() });
   }
 
   // Build request params
-  const { datalabName, notebookId, notebookType } = matchedData(req);
+  const { datalabName, notebookId, notebookType } = matchedData(request);
 
   // Handle request
   return notebookManager.createNotebook(datalabName, notebookId, notebookType)
     .then(() => {
-      res.status(201);
-      res.send({ message: 'OK' });
+      response.status(201);
+      response.send({ message: 'OK' });
     })
-    .catch((error) => {
-      logger.error(error);
-      res.status(500);
-      res.send({ error });
-    });
+    .catch(handleError(response, notebookId));
 }
+
+const handleError = (res, notebookId) => (error) => {
+  logger.error(error);
+  res.status(500);
+  res.send({
+    message: `Error creating notebook ${notebookId}`,
+    error: error.message });
+};
 
 const createNotebookValidator = [
   check('datalabName').exists().withMessage('datalabName must be specified'),
--- code/infrastructure-api/src/controllers/notebookController.spec.js
+++ code/infrastructure-api/src/controllers/notebookController.spec.js
@@ -1,4 +1,5 @@
 import httpMocks from 'node-mocks-http';
+import Promise from 'bluebird';
 import notebookController from './notebookController';
 import notebookManager from '../notebooks/notebookManager';
 
@@ -7,16 +8,13 @@ jest.mock('../notebooks/notebookManager');
 const createNotebookMock = jest.fn();
 notebookManager.createNotebook = createNotebookMock;
 
+let request;
+
 describe('Notebook Controller', () => {
+  beforeEach(() => createValidatedRequest());
+
   it('should process a valid request', () => {
     createNotebookMock.mockReturnValue(Promise.resolve());
-
-    const request = httpMocks.createRequest({
-      method: 'GET',
-      body: createRequestBody(),
-      _validationErrors: [], // No errors from middleware
-    });
-
     const response = httpMocks.createResponse();
 
     return notebookController.createNotebook(request, response)
@@ -26,7 +24,7 @@ describe('Notebook Controller', () => {
   });
 
   it('should return 400 for invalid request', () => {
-    const request = httpMocks.createRequest({
+    const invalidRequest = httpMocks.createRequest({
       method: 'GET',
       body: createRequestBody(),
       _validationErrors: [{
@@ -38,7 +36,7 @@ describe('Notebook Controller', () => {
 
     const response = httpMocks.createResponse();
 
-    notebookController.createNotebook(request, response);
+    notebookController.createNotebook(invalidRequest, response);
     expect(response.statusCode).toBe(400);
     const expectedError = {
       errors: {
@@ -53,19 +51,14 @@ describe('Notebook Controller', () => {
   });
 
   it('should return 500 for failed request', () => {
-    createNotebookMock.mockReturnValue(Promise.reject('error'));
-    const request = httpMocks.createRequest({
-      method: 'GET',
-      body: createRequestBody(),
-      _validationErrors: [],
-    });
+    createNotebookMock.mockReturnValue(Promise.reject({ message: 'error' }));
 
     const response = httpMocks.createResponse();
 
-    notebookController.createNotebook(request, response)
+    return notebookController.createNotebook(request, response)
       .then(() => {
         expect(response.statusCode).toBe(500);
-        expect(response._getData()).toEqual({ error: 'error' }); // eslint-disable-line no-underscore-dangle
+        expect(response._getData()).toEqual({ error: 'error', message: 'Error creating notebook notebookId' }); // eslint-disable-line no-underscore-dangle
       });
   });
 });
@@ -77,3 +70,15 @@ function createRequestBody() {
     notebookType: 'jupyter',
   };
 }
+
+function createValidatedRequest() {
+  request = httpMocks.createRequest({
+    method: 'GET',
+    body: createRequestBody(),
+  });
+
+  const validators = notebookController.createNotebookValidator.map(createValidationPromise(request));
+  return Promise.all(validators);
+}
+
+const createValidationPromise = req => validator => new Promise((resolve) => { validator(req, null, resolve); });
