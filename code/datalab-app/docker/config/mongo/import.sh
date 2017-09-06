2d94620b8e2c51b0c2428e6543e58e5dbea8c134
Gary Lloyd <gareth.lloyd@tessella.com>
2017-09-06 17:40:45 +0100

    Feature: Add datalab collection to test MongoDB

--- code/datalab-app/docker/config/mongo/import.sh
+++ code/datalab-app/docker/config/mongo/import.sh
@@ -4,6 +4,7 @@ set -e
 
 echo "Job Import started: $(date)"
 
+mongoimport --quiet --host $MONGO_HOST:$MONGO_PORT --db $MONGO_DB_NAME --collection datalabs --type json --drop --file /tmp/mongodb/datalabCollection.json --jsonArray
 mongoimport --quiet --host $MONGO_HOST:$MONGO_PORT --db $MONGO_DB_NAME --collection notebooks --type json --drop --file /tmp/mongodb/notebookCollection.json --jsonArray
 mongoimport --quiet --host $MONGO_HOST:$MONGO_PORT --db $MONGO_DB_NAME --collection dataStorage --type json --drop --file /tmp/mongodb/dataStorageCollection.json --jsonArray
 
