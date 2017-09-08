c31a8e3a80a9200ec79095127cde33222c2f30d8
Gary Lloyd <gareth.lloyd@tessella.com>
2017-09-08 14:14:25 +0100

    Fix: Remove console.log

--- code/datalab-app/src/app/api/notebookService.js
+++ code/datalab-app/src/app/api/notebookService.js
@@ -22,7 +22,6 @@ function getUrl(id) {
 
 function createNotebook(notebook) {
   const mutation = { query: `mutation { createNotebook(notebook: { name: "${notebook.name}", notebookType: ${notebook.type}}) { name } }` };
-  console.log(mutation);
   return request.post(apiURL, mutation)
     .then(res => get(res, 'data.data.notebook'));
 }
