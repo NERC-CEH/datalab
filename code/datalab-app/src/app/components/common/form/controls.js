2ac50f497852fdabd60ab8b7cbb9d30ffa367acf
Gary Lloyd <gareth.lloyd@tessella.com>
2017-09-18 18:36:58 +0100

    Fix: Remove unnecessary error fields

--- code/datalab-app/src/app/components/common/form/controls.js
+++ code/datalab-app/src/app/components/common/form/controls.js
@@ -1,25 +1,25 @@
 import React from 'react';
-import { Input, TextArea, Dropdown, Form, Label } from 'semantic-ui-react';
+import { Input, Dropdown, Form, Label } from 'semantic-ui-react';
 import { fieldStyle, labelStyle } from './controlStyles';
 
 export const renderTextField = ({ input, label, meta: { touched, error }, ...custom }) =>
   <Form.Field error={error && touched}>
     <label>{label}</label>
-    <Input type='text' style={fieldStyle} {...input} {...custom} error={error && touched}/>
+    <Input type='text' style={fieldStyle} {...input} {...custom}/>
     {touched && (error && <Label style={labelStyle} pointing>{error}</Label>)}
   </Form.Field>;
 
 export const renderTextArea = ({ input, label, meta: { touched, error }, ...custom }) =>
   <Form.Field error={error && touched}>
     <label>{label}</label>
-    <TextArea style={fieldStyle} {...input} {...custom} error={error && touched}/>
+    <textarea style={fieldStyle} {...input} {...custom}/>
     {touched && (error && <Label style={labelStyle} pointing>{error}</Label>)}
   </Form.Field>;
 
 export const renderDropdownField = ({ input, label, meta: { touched, error }, ...custom }) =>
   <Form.Field error={error && touched}>
     <label>{label}</label>
-    <Dropdown fluid selection {...input} {...custom} value={input.value} error={error && touched}
+    <Dropdown fluid selection {...input} {...custom} value={input.value}
               onChange={ (param, data) => input.onChange(data.value)}/>
     {touched && (error && <Label style={labelStyle} pointing>{error}</Label>)}
   </Form.Field>;
