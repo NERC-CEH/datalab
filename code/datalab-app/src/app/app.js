8a0247896c1cdd4f072082f28fadad38bb7230e8
Joshua Foster <joshua.foster@tessella.com>
2017-10-05 15:19:49 +0100

    Feature: Add welcome page hero bar

--- code/datalab-app/src/app/app.js
+++ code/datalab-app/src/app/app.js
@@ -1,21 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import { MuiThemeProvider, createMuiTheme } from 'material-ui';
 import configureStore from './store/configureStore';
 import Router from './router';
 
 const store = configureStore();
 
-const theme = createMuiTheme({
-  palette: {},
-});
-
 ReactDOM.render(
   <Provider store={store}>
-    <MuiThemeProvider theme={theme}>
       <Router />
-    </MuiThemeProvider>
   </Provider>,
   document.getElementById('root'),
 );
