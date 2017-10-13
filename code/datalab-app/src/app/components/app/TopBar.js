8c6ece009026a46251a0924800b3e628b6fa5339
Joshua Foster <joshua.foster@tessella.com>
2017-10-13 11:26:48 +0100

    Chore: Remove unused imports

--- code/datalab-app/src/app/components/app/TopBar.js
+++ code/datalab-app/src/app/components/app/TopBar.js
@@ -2,8 +2,6 @@ import React from 'react';
 import AppBar from 'material-ui/AppBar';
 import Toolbar from 'material-ui/Toolbar';
 import Button from 'material-ui/Button';
-import IconButton from 'material-ui/IconButton';
-import Menu, { MenuItem } from 'material-ui/Menu';
 import auth from '../../auth/auth';
 
 const TopBar = () => (
