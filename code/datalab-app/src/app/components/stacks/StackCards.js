7f5283e181a20f3573b5767470e1a58e86fb7061
Joshua Foster <joshua.foster@tessella.com>
2017-11-20 10:21:04 +0000

    Chore: Correct typo

--- code/datalab-app/src/app/components/stacks/StackCards.js
+++ code/datalab-app/src/app/components/stacks/StackCards.js
@@ -4,16 +4,16 @@ import Grid from 'material-ui/Grid';
 import StackCard from './StackCard';
 import CreateStackContainer from '../../containers/stacks/CreateStackContainer';
 
-const brakePoints = { xs: 12, sm: 6, md: 4, lg: 4, xl: 2 };
+const breakPoints = { xs: 12, sm: 6, md: 4, lg: 4, xl: 2 };
 
 const StackCards = ({ stacks, openStack, deleteStack, typeName, containerType, dialogAction, formStateName }) => (
   <Grid container>
     {stacks.map((stack, index) => (
-      <Grid key={index} item {...brakePoints}>
+      <Grid key={index} item {...breakPoints}>
         <StackCard stack={stack} openStack={openStack} deleteStack={deleteStack} typeName={typeName} />
       </Grid>
     ))}
-    <Grid item {...brakePoints}>
+    <Grid item {...breakPoints}>
       <CreateStackContainer typeName={typeName} containerType={containerType} dialogAction={dialogAction} formStateName={formStateName} />
     </Grid>
   </Grid>
