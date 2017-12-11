94899f424be3262da2ca42f4130c4144f1b0e06e
Joshua Foster <joshua.foster@tessella.com>
2017-12-11 13:39:59 +0000

    Feature: Add post & delete calls to infrastructure api

--- code/datalab-app/src/api/infrastructure/dataStoreApi.js
+++ code/datalab-app/src/api/infrastructure/dataStoreApi.js
@@ -1,8 +1,11 @@
+import axios from 'axios';
 import logger from 'winston';
 import axiosErrorHandler from '../util/errorHandlers';
 import datalabRepository from '../dataaccess/datalabRepository';
 import dataStorageRepository from '../dataaccess/dataStorageRepository';
+import config from '../config';
 
+const VOLUME_URL = `${config.get('infrastructureApi')}/volumes`;
 const CREATE_ERROR_MESSAGE = 'Unable to create Data Store';
 const DELETE_ERROR_MESSAGE = 'Unable to delete Data Store';
 
@@ -21,10 +24,43 @@ function deleteDataStore(user, datalabName, dataStore) {
     .catch(axiosErrorHandler(DELETE_ERROR_MESSAGE));
 }
 
-const storeAndCreateDataStore = (user, dataStore) => datalabInfo =>
-  dataStorageRepository.createOrUpdate(user, dataStore).then(() => dataStore);
+const storeAndCreateDataStore = (user, dataStore) => (datalabInfo) => {
+  const volumeRequest = {
+    ...dataStore,
+    linkToStorage: `http://${dataStore.name}-minio.${datalabInfo.domain}/minio`,
+    internalEndpoint: `http://${dataStore.name}-minio.${datalabInfo.domain}/minio`,
+  };
+
+  return dataStorageRepository.createOrUpdate(user, volumeRequest)
+    .then(sendCreationRequest(volumeRequest, datalabInfo))
+    .then(() => dataStore);
+};
+
+const sendCreationRequest = (datalabRequest, datalabInfo) => () => {
+  const payload = {
+    datalabInfo,
+    name: datalabRequest.name,
+    volumeSize: datalabRequest.capacityTotal,
+  };
+
+  logger.debug(`Create Request Url: ${VOLUME_URL} payload ${JSON.stringify(payload)}`);
+  return axios.post(VOLUME_URL, payload)
+    .then(response => logger.info(response.data));
+};
 
 const removeAndDeleteDataStore = (user, dataStore) => datalabInfo =>
-  dataStorageRepository.deleteByName(user, dataStore);
+  sendDeletionRequest(user, dataStore, datalabInfo)
+    .then(dataStorageRepository.deleteByName(user, dataStore));
+
+const sendDeletionRequest = (user, dataStore, datalabInfo) => {
+  const payload = {
+    datalabInfo,
+    name: dataStore.name,
+  };
+
+  logger.debug(`Deletion Request Url: ${VOLUME_URL} payload ${JSON.stringify(payload)}`);
+  return axios.delete(VOLUME_URL, { data: payload })
+    .then(response => logger.info(response.data));
+};
 
 export default { createDataStore, deleteDataStore };
