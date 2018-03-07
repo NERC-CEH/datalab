ce495436a91dbf5ce84f07f3f5c3f9983bf2bc71
Joshua Foster <joshua.foster@tessella.com>
2018-03-07 18:08:57 +0000

    Feature: Add permissions to routing

--- code/datalab-app/src/app/PrivateApp.js
+++ code/datalab-app/src/app/PrivateApp.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import { Route, Switch } from 'react-router-dom';
 import NavigationContainer from './containers/app/NavigationContainer';
 import NotFoundPage from './pages/NotFoundPage';
@@ -11,20 +12,37 @@ import DaskPage from './pages/DaskPage';
 import SparkPage from './pages/SparkPage';
 import ModalRoot from './containers/modal/ModalRoot';
 
-const PrivateApp = () => (
-  <NavigationContainer>
-    <Switch>
-      <Route exact path="/" component={LandingPage} />
-      <Route exact path="/storage" component={DataStoragePage} />
-      <Route exact path="/notebooks" component={NotebooksPage} />
-      <Route exact path="/publishing" component={PublishingPage} />
-      <Route exact path="/dask" component={DaskPage} />
-      <Route exact path="/spark" component={SparkPage} />
-      <Route component={NotFoundPage} />
-    </Switch>
-    <Route component={Footer} />
-    <ModalRoot />
-  </NavigationContainer>
-);
+const PrivateApp = ({ userPermissions }) => {
+  const ListStorage = PermissionWrapper('project:storage:list', userPermissions);
+  const ListStacks = PermissionWrapper('project:stacks:list', userPermissions);
+
+  return (
+    <NavigationContainer>
+      <Switch>
+        <Route exact path="/" component={LandingPage} />
+        <Route exact path="/storage" render={ListStorage(DataStoragePage)} />
+        <Route exact path="/notebooks" render={ListStacks(NotebooksPage)} />
+        <Route exact path="/publishing" render={ListStacks(PublishingPage)} />
+        <Route exact path="/dask" component={DaskPage} />
+        <Route exact path="/spark" component={SparkPage} />
+        <Route component={NotFoundPage} />
+      </Switch>
+      <Route component={Footer} />
+      <ModalRoot />
+    </NavigationContainer>
+  );
+};
+
+const PermissionWrapper = (permission, userPermissions) => (WrappedComponent) => {
+  if (userPermissions.includes(permission)) {
+    return props => (<WrappedComponent userPermissions={userPermissions} {...props} />);
+  }
+
+  return () => (<NotFoundPage />);
+};
+
+PrivateApp.propTypes = {
+  userPermissions: PropTypes.arrayOf(PropTypes.string).isRequired,
+};
 
 export default PrivateApp;
