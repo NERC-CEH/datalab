d373c0c7e9e9e21789e383e0c38c424be964a977
Joshua Foster <joshua.foster@tessella.com>
2018-03-21 11:50:25 +0000

    Refactor: Remove permission string values

--- code/infrastructure-api/src/config/routes.js
+++ code/infrastructure-api/src/config/routes.js
@@ -5,20 +5,28 @@ import volume from '../controllers/volumeController';
 import verifyToken from '../auth/authMiddleware';
 import permissionWrapper from '../auth/permissionMiddleware';
 
+const STACKS_LIST = 'stacks:list';
+const STACKS_OPEN = 'stacks:open';
+const STACKS_CREATE = 'stacks:create';
+const STACKS_DELETE = 'stacks:delete';
+const STORAGE_LIST = 'storage:list';
+const STORAGE_CREATE = 'storage:create';
+const STORAGE_DELETE = 'storage:delete';
+
 function configureRoutes(app) {
   app.get('/status', status.status);
   app.all('*', verifyToken); // Routes above this line are not auth checked
-  app.get('/stacks', permissionWrapper('stacks:list'), stacks.coreStacksValidator, stacks.listStacks);
-  app.get('/stacks/category/:category', permissionWrapper('stacks:list'), stacks.withCategoryValidator, stacks.listByCategory);
-  app.get('/stacks/mount/:mount', permissionWrapper('storage:delete'), stacks.withMountValidator, stacks.listByMount);
-  app.delete('/stack', permissionWrapper('stacks:delete'), stack.deleteStackValidator, stack.deleteStack);
-  app.get('/stack/id/:id', permissionWrapper('stacks:open'), stack.withIdValidator, stack.getOneById);
-  app.get('/stack/name/:name', permissionWrapper('stacks:create'), stack.withNameValidator, stack.getOneByName);
-  app.post('/stack', permissionWrapper('stacks:create'), stack.createStackValidator, stack.createStack);
-  app.post('/volume', permissionWrapper('storage:create'), volume.createVolumeValidator, volume.createVolume);
-  app.delete('/volume', permissionWrapper('storage:delete'), volume.coreVolumeValidator, volume.deleteVolume);
-  app.post('/volume/query', permissionWrapper('storage:list'), volume.coreVolumeValidator, volume.queryVolume);
-  app.get('/volumes', permissionWrapper('storage:list'), volume.listVolumes);
+  app.get('/stacks', permissionWrapper(STACKS_LIST), stacks.listStacks);
+  app.get('/stacks/category/:category', permissionWrapper(STACKS_LIST), stacks.withCategoryValidator, stacks.listByCategory);
+  app.get('/stacks/mount/:mount', permissionWrapper(STORAGE_DELETE), stacks.withMountValidator, stacks.listByMount);
+  app.get('/stack/id/:id', permissionWrapper(STACKS_OPEN), stack.withIdValidator, stack.getOneById);
+  app.get('/stack/name/:name', permissionWrapper(STACKS_CREATE), stack.withNameValidator, stack.getOneByName);
+  app.delete('/stack', permissionWrapper(STACKS_DELETE), stack.deleteStackValidator, stack.deleteStack);
+  app.post('/stack', permissionWrapper(STACKS_CREATE), stack.createStackValidator, stack.createStack);
+  app.post('/volume/query', permissionWrapper(STORAGE_LIST), volume.coreVolumeValidator, volume.queryVolume);
+  app.get('/volumes', permissionWrapper(STORAGE_LIST), volume.listVolumes);
+  app.post('/volume', permissionWrapper(STORAGE_CREATE), volume.createVolumeValidator, volume.createVolume);
+  app.delete('/volume', permissionWrapper(STORAGE_DELETE), volume.coreVolumeValidator, volume.deleteVolume);
 }
 
 export default { configureRoutes };
