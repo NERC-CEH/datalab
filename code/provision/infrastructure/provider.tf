268261800d2df85d0b3c71f3b2faf2f87f2bd7b9
Gary Lloyd <gareth.lloyd@tessella.com>
2018-05-04 19:53:53 +0100

    Feature: Add terraform http backend template configuration

--- code/provision/infrastructure/provider.tf
+++ code/provision/infrastructure/provider.tf
@@ -1,3 +1,13 @@
+terraform {
+  backend "http" {
+    address = "http://<ip_address>/state/datalabs"
+    lock_address = "http://<ip_address>/state/datalabs"
+    unlock_address = "http://<ip_address>/state/datalabs"
+    username = "username"
+    password = "password"
+  }
+}
+
 provider "openstack" {
   cloud = "jasmin"
   insecure = true
