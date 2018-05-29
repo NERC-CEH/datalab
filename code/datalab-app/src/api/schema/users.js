c453e0e8352ebd2fac67e4a1cfc56762a9fe8e26
Joshua Foster <joshua.foster@tessella.com>
2018-05-29 13:48:03 +0100

    Feature: Query auth service for users

--- code/datalab-app/src/api/schema/users.js
+++ code/datalab-app/src/api/schema/users.js
@@ -2,6 +2,7 @@ import { GraphQLList } from 'graphql';
 import UserType from '../types/userTypes';
 import permissionChecker from '../auth/permissionChecker';
 import { usersPermissions } from '../../shared/permissionTypes';
+import userService from '../dataaccess/usersService';
 
 const { USERS_LIST } = usersPermissions;
 
@@ -9,7 +10,7 @@ const users = {
   description: 'List of users within the current DataLab',
   type: new GraphQLList(UserType),
   resolve: (obj, args, { user, token }) =>
-    permissionChecker(USERS_LIST, user, () => [{ name: 'name', userId: 'userId' }]),
+    permissionChecker(USERS_LIST, user, () => userService.getAll({ token })),
 };
 
 export default users;
