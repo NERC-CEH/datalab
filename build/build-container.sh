981e4423006b8f05a13e161a689f5406101553e3
Iain Walmsley <iainw@ceh.ac.uk>
2018-12-12 12:12:14 +0000

    t# This is a combination of 4 commits. HOTFIX: Add Image to Build Stage

--- build/build-container.sh
+++ build/build-container.sh
@@ -7,41 +7,41 @@ if [[ ($# -eq 1 || $# -eq 2 && $2 == "--push" ) ]] && [[ "$1" =~ ^(docs|api|app|
   case "$1" in
   docs)
     echo "Starting to build documents..."
-    cd ./docs && yarn dist
+    cd ./docs && yarn install && yarn dist
     DOCKERFILE="Dockerfile"
     IMAGE="docs"
     ;;
   api)
     echo "Starting to build datalab-api..."
-    cd ./code/datalab-app && yarn dist-create && yarn build-api
+    cd ./code/datalab-app && yarn install && yarn dist-create && yarn build-api
     DOCKERFILE="api.Dockerfile"
     IMAGE="datalab-api"
     ;;
   app)
     echo "Starting to build datalab-app..."
-    cd ./code/datalab-app && yarn dist-create && yarn build-app
+    cd ./code/datalab-app && yarn install && yarn dist-create && yarn build-app
     DOCKERFILE="app.Dockerfile"
     IMAGE="datalab-app"
     ;;
   infrastructure)
     echo "Starting to build infrastructure-api..."
-    cd ./code/infrastructure-api && yarn dist
+    cd ./code/infrastructure-api && yarn install && yarn dist
     DOCKERFILE="Dockerfile"
     IMAGE="infrastructure-api"
     ;;
   authorisation)
     echo "Starting to build auth-service..."
-    cd ./code/auth-service && yarn dist
+    cd ./code/auth-service && yarn install && yarn dist
     DOCKERFILE="Dockerfile"
     IMAGE="authorisation-svc"
     ;;
   esac
   echo "Generating docker image..."
-  docker build -f $DOCKERFILE -t nerc/$IMAGE:latest .
-  docker tag nerc/$IMAGE:latest nerc/$IMAGE:$GIT_DESCRIBE
+  docker build -f $DOCKERFILE -t nercceh/$IMAGE:latest .
+  docker tag nercceh/$IMAGE:latest nercceh/$IMAGE:$GIT_DESCRIBE
   if [ "$#" -eq 2 ]; then
     echo "Attempting to push image..."
-    docker push nerc/$IMAGE
+    docker push nercceh/$IMAGE
   fi
   echo "Success!"
 else
