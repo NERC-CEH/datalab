61bcb5087dfe856fedac74e053fbef6d4d2b6263
Iain Walmsley <iainw@ceh.ac.uk>
2019-05-24 15:35:49 +0100

    Incrementing RShiny to 3.5.3

--- code/infrastructure-api/src/kubernetes/deploymentGenerator.js
+++ code/infrastructure-api/src/kubernetes/deploymentGenerator.js
@@ -17,7 +17,7 @@ const RSTUDIO_CONNECT_IMAGE = 'nerc/zeppelin-connect'; // This name should be ze
 const RSTUDIO_CONNECT_VERSION = '1.1.1';
 
 const RSHINY_IMAGE = 'nercceh/rshiny';
-const RSHINY_VERSION = '0.0.1-5-g9a5617f';
+const RSHINY_VERSION = '0.0.1-6-g5557f8f';
 
 const NBVIEWER_IMAGE = 'nercceh/jupyter-nbviewer';
 const NBVIEWER_VERSION = '0.0.1-1-g129dec8';
