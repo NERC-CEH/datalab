6a286b7ea0ad835d253e495143de6270c5af700f
Alex Lee <alex.lee@tessella.com>
2019-07-19 11:16:23 +0100

    chore: update test config and set up for new enzyme version

--- code/datalab-app/src/setupTests.js
+++ code/datalab-app/src/setupTests.js
@@ -1,1 +1,5 @@
 import 'jest-enzyme';// eslint-disable-line no-unused-vars
+import Enzyme from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+
+Enzyme.configure({ adapter: new Adapter() });
