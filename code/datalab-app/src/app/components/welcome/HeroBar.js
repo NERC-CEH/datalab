5bcf250496167f340cd6ad02eb983f29fd486a2f
Alex Lee <alex.lee@tessella.com>
2019-07-31 16:59:36 +0100

    feat: enable Auth0 signup through the UI

--- code/datalab-app/src/app/components/welcome/HeroBar.js
+++ code/datalab-app/src/app/components/welcome/HeroBar.js
@@ -31,7 +31,7 @@ const HeroBar = ({ classes }) => (
   <div className={classes.bar}>
     <img className={classes.logo} src={datalabsLogo} alt="DataLabs-Logo" />
     <Typography className={classes.tagLine} variant="h6">{tagLine}</Typography>
-    <Button className={classes.button} color="primary" disabled>Sign Up</Button>
+    <Button className={classes.button} color="primary" onClick={getAuth().signUp}>Sign Up</Button>
     <Button className={classes.button} color="primary" onClick={getAuth().login}> Log In</Button>
   </div>
 );
