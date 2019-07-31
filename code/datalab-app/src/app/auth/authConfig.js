540d33e98e6f4c470be0a5182660cf245cafda02
Joshua Foster <joshua.foster@tessella.com>
2019-07-31 09:23:17 +0100

    fix: update auth config for local environment

--- code/datalab-app/src/app/auth/authConfig.js
+++ code/datalab-app/src/app/auth/authConfig.js
@@ -2,6 +2,16 @@ import axios from 'axios';
 
 const getAuthConfig = () => axios
   .get('/web_auth_config.json')
-  .then(({ data }) => data);
+  .then(({ data }) => {
+    if (window.location.hostname === 'localhost') {
+      return {
+        ...data,
+        redirectUri: `${window.location.origin}/callback`,
+        returnTo: `${window.location.origin}/`,
+      };
+    }
+
+    return data;
+  });
 
 export default getAuthConfig;
