9121dc8c8ff9c658e3f155dea84fc12d43e4dd4f
Andrew Longmore (Tessella) <andrew.longmore@tessella.com>
2019-08-19 16:57:26 +0100

    Make "project" role just an eg of a project name.

--- code/auth-service/src/models/userRoles.model.js
+++ code/auth-service/src/models/userRoles.model.js
@@ -2,21 +2,17 @@ import mongoose from 'mongoose';
 
 const { Schema } = mongoose;
 
-export const INSTANCE_ADMIN = 'instance-admin';
 export const PROJECT_ADMIN = 'admin';
 export const PROJECT_USER = 'user';
 export const PROJECT_VIEWER = 'viewer';
 
-const instanceRoles = [INSTANCE_ADMIN];
 const projectRoles = [PROJECT_ADMIN, PROJECT_USER, PROJECT_VIEWER];
 
 const UserRolesSchema = new Schema({
   userId: String,
-  instanceRoles: [{
-    role: { type: String, enum: instanceRoles },
-  }],
-  projectRoles: [{
-    project: String,
+  instanceAdmin: Boolean,
+  roles: [{
+    projectName: String,
     role: { type: String, enum: projectRoles },
   }],
 });
--- code/auth-service/src/permissions/permissions.js
+++ code/auth-service/src/permissions/permissions.js
@@ -6,43 +6,23 @@ const roleDelim = ':';
 
 const permissionAttributes = yaml.safeLoad(fs.readFileSync(config.get('permissionAttributes')));
 
-//    [ { role: 'instance-admin' }, { role: 'legacy-project:admin' } ]
-// -> [ 'instance-admin', 'project:admin' ]
-const extractInstanceRoles = entry => entry.role.replace('legacy-project', 'project');
-
-//    [ 'instance-admin', 'project:admin' ]
-// -> [ ['instance-admin'], ['project','admin'] ]
-const parseRoles = role => role.split(roleDelim, 2);
-
-//    [ ['instance-admin'], ['project','admin'] ]
-// -> [ { role: 'instance-admin', projectName: undefined }, { role: 'admin', projectName: 'project' } ]
-const parseProjects = splitRoles => ({
-  role: splitRoles.pop(),
-  projectName: splitRoles.pop(),
-});
-
-//    [ { role: 'instance-admin', projectName: undefined }, { role: 'admin', projectName: 'project' } ]
-// -> [ { role: 'instance-admin', projectName: undefined, permissions: [] },
-//      { role: 'admin', projectName: 'project', permissions: [stackAttributes, storageAttributes, userAttributes] } ]
+//    [ { role: 'admin', projectName: 'project' } ]
+// -> [ { role: 'admin', projectName: 'project', permissions: [stackAttributes, storageAttributes, userAttributes] } ]
 const getPermissions = roleObject => ({
   ...roleObject,
   permissions: permissionAttributes[roleObject.role] || [],
 });
 
-//    [ { role: 'instance-admin', projectName: undefined, permissions: [] },
-//      { role: 'admin', projectName: 'project', permissions: [stackAttributes, storageAttributes, userAttributes] } ]
-// -> [ { role: 'instance-admin', projectName: undefined, permissions: [] },
-//      { role: 'admin', projectName: 'project', permissions: [/see permissions.yml/] } ]
+//    [ { role: 'admin', projectName: 'project', permissions: [stackAttributes, storageAttributes, userAttributes] } ]
+// -> [ { role: 'admin', projectName: 'project', permissions: [/see permissions.yml/] } ]
 const buildPermissions = ({ permissions, ...rest }) => ({
   ...rest,
   permissions: permissions.map(({ name, permissions: subPermissions }) => subPermissions.map(permission => name.concat(roleDelim, permission)))
     .reduce(flattenArray, []),
 });
 
-//    [ { role: 'instance-admin', projectName: undefined, permissions: [] },
-//      { role: 'admin', projectName: 'project', permissions: [/see permissions.yml/] } ]
-// -> [ [],
-//      ['project:stacks:delete', ...] ]
+//    [ { role: 'admin', projectName: 'project', permissions: [/see permissions.yml/] } ]
+// -> [ ['project:stacks:delete', ...] ]
 const projectifyPermissions = ({ projectName, permissions }) => {
   const project = projectName ? `${projectName}${roleDelim}` : '';
 
@@ -56,20 +36,10 @@ const flattenArray = (previous, current) => {
   return previous;
 };
 
-function processRoles(roles) {
-  const legacyProjectRoles = roles.instanceRoles
-    .map(extractInstanceRoles)
-    .map(parseRoles)
-    .map(parseProjects);
-
-  const projectRoles = legacyProjectRoles.concat(roles.projectRoles);
-
-  const projectPermissions = projectRoles
-    .map(getPermissions)
-    .map(buildPermissions)
-    .map(projectifyPermissions);
-
-  return projectPermissions.reduce(flattenArray, []);
-}
+const processRoles = userRoles => userRoles.roles
+  .map(getPermissions)
+  .map(buildPermissions)
+  .map(projectifyPermissions)
+  .reduce(flattenArray, []);
 
 export default processRoles;
