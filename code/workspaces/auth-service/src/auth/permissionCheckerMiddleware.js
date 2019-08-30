f27748d0e0be7807b8604aeaa020a72b3ab7e7fd
Gary Lloyd <gareth.lloyd@tessella.com>
2019-08-30 08:45:14 +0100

    feat: extend permission check to include project

--- code/workspaces/auth-service/src/auth/permissionCheckerMiddleware.js
+++ code/workspaces/auth-service/src/auth/permissionCheckerMiddleware.js
@@ -4,13 +4,29 @@ import logger from 'winston';
 const PROJECT = 'project';
 const permissionDelim = ':';
 
-function permissionWrapper(permissionSuffixes) {
-  const requiredPermissions = permissionSuffixes.map(suffix => PROJECT.concat(permissionDelim, suffix));
+function permissionChecker(permissionSuffixes) {
+  const projectName = () => PROJECT;
+  return checkPermissions(permissionSuffixes, projectName);
+}
 
+function projectPermissionChecker(permissionSuffixes) {
+  const projectName = request => get(request, 'params.projectName');
+  return checkPermissions(permissionSuffixes, projectName);
+}
+
+function checkPermissions(permissionSuffixes, projectNameFn) {
   return (request, response, next) => {
+    const projectName = projectNameFn(request);
+    if (!projectName) {
+      logger.warn('Auth: permission check: FAILED');
+      response.status(401);
+      return response.send({ message: 'Request missing parameter: projectName' });
+    }
+
+    const requiredPermissions = permissionSuffixes.map(suffix => projectName.concat(permissionDelim, suffix));
     const grantedPermissions = get(request, 'user.permissions') || [];
 
-    logger.info('Auth: checking permissions');
+    logger.debug('Auth: checking permissions');
     logger.debug(`Auth: expected permission(s): ${requiredPermissions}`);
     logger.debug(`Auth: granted user permission(s): ${grantedPermissions}`);
 
@@ -20,11 +36,11 @@ function permissionWrapper(permissionSuffixes) {
       return response.send({ message: `User missing expected permission(s): ${requiredPermissions}` });
     }
 
-    logger.info('Auth: permission check: PASSED');
+    logger.debug('Auth: permission check: PASSED');
     return next();
   };
 }
 
 const arraysIncludes = (current, expected) => current.some(currentValue => expected.some(expectedValue => new RegExp(`^${currentValue}$`).test(expectedValue)));
 
-export default permissionWrapper;
+export { permissionChecker, projectPermissionChecker };
