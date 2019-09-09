99b33a662984a91d62f4da74a6430a7838f3eac1
Gary Lloyd <gareth.lloyd@tessella.com>
2019-09-09 10:18:52 +0100

    chore: update graphql express server

--- code/workspaces/client-api/src/config/graphql.js
+++ code/workspaces/client-api/src/config/graphql.js
@@ -1,37 +1,23 @@
-import { graphqlExpress, graphiqlExpress } from 'graphql-server-express';
+import { ApolloServer } from 'apollo-server-express';
 import { importSchema } from 'graphql-import';
 import { join } from 'path';
-import { makeExecutableSchema } from 'graphql-tools';
-import { authorise, verifyToken } from '../auth/authMiddleware';
-import status from '../status';
+
 import resolvers from '../schema/resolvers';
 
 function configureGraphQL(app) {
   const typeDefs = importSchema(join(__dirname, '..', 'schema', 'schema.graphql'));
-  const schema = makeExecutableSchema({
+
+  const server = new ApolloServer({
     typeDefs,
     resolvers,
+    context: ({ req }) => ({
+      user: req.user,
+      token: req.headers.authorization,
+    }),
+    playground: process.env.NODE_ENV !== 'production',
   });
 
-  const api = graphqlExpress(request => ({
-    schema,
-    context: {
-      user: request.user,
-      token: request.headers.authorization,
-    },
-  }));
-
-  const graphiql = graphiqlExpress({
-    endpointURL: '/graphiqlApi',
-  });
-
-  app.use('/api', authorise, verifyToken, api);
-  app.get('/status', status.get);
-
-  if (process.env.NODE_ENV !== 'production') {
-    app.use('/graphiqlApi', api);
-    app.use('/graphiql', graphiql);
-  }
+  server.applyMiddleware({ app, path: '/api' });
 }
 
 export default { configureGraphQL };
