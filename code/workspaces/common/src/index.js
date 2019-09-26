3114c590a8831e4b0ca4a646fc6263987b7e9ef9
Gary Lloyd <gareth.lloyd@tessella.com>
2019-09-26 10:35:40 +0100

    chore: remove service-chassis code from common

--- code/workspaces/common/src/index.js
+++ code/workspaces/common/src/index.js
@@ -1,11 +1,9 @@
 import * as permissionTypes from './permissionTypes';
 import * as stackTypes from './stackTypes';
 import * as statusTypes from './statusTypes';
-import * as service from './service';
 
 export {
   permissionTypes,
   stackTypes,
   statusTypes,
-  service,
 };
