1f174a1e65bd7164b2148e40baf943fb8c34e1e2
Alex Lee <alex.lee@tessella.com>
2019-11-13 16:38:46 +0000

    feat(web app): update to only allow users in stack users list to delete stack

--- code/workspaces/web-app/src/components/stacks/StackCardActions.js
+++ code/workspaces/web-app/src/components/stacks/StackCardActions.js
@@ -2,6 +2,7 @@ import { withStyles } from '@material-ui/core/styles';
 import PropTypes from 'prop-types';
 import React from 'react';
 import { statusTypes } from 'common';
+import { useCurrentUserId } from '../../hooks/authHooks';
 import PermissionWrapper from '../common/ComponentPermissionWrapper';
 import PrimaryActionButton from '../common/buttons/PrimaryActionButton';
 import SecondaryActionButton from '../common/buttons/SecondaryActionButton';
@@ -21,38 +22,47 @@ const styles = theme => ({
   },
 });
 
-const StackCardActions = ({ stack, openStack, deleteStack, editStack, userPermissions, openPermission,
-  deletePermission, editPermission, classes }) => (
-  <div className={classes.cardActions}>
-    {openStack && <PermissionWrapper className={classes.buttonWrapper} userPermissions={userPermissions} permission={openPermission}>
-      <PrimaryActionButton
-        disabled={!isReady(stack)}
-        onClick={() => openStack(stack)}
-        fullWidth
-      >
-        Open
-      </PrimaryActionButton>
-    </PermissionWrapper>}
-    {deleteStack && <PermissionWrapper className={classes.buttonWrapper} userPermissions={userPermissions} permission={deletePermission}>
-      <SecondaryActionButton
-        disabled={!isReady(stack)}
-        onClick={() => deleteStack(stack)}
-        fullWidth
-      >
-        Delete
-      </SecondaryActionButton>
-    </PermissionWrapper>}
-    {editStack && <PermissionWrapper className={classes.buttonWrapper} userPermissions={userPermissions} permission={editPermission}>
-      <SecondaryActionButton
-        disabled={!isReady(stack)}
-        onClick={() => editStack(stack)}
-        fullWidth
-      >
-        Edit
-      </SecondaryActionButton>
-    </PermissionWrapper>}
-  </div>
-);
+const StackCardActions = (props) => {
+  const currentUserId = useCurrentUserId();
+  return <PureStackCardActions currentUserId={currentUserId} {...props} />;
+};
+
+export const PureStackCardActions = ({ stack, openStack, deleteStack, editStack, userPermissions, openPermission,
+  deletePermission, editPermission, currentUserId, classes }) => {
+  const ownsNotebook = stack.users.includes(currentUserId);
+
+  return (
+    <div className={classes.cardActions}>
+      {openStack && <PermissionWrapper className={classes.buttonWrapper} userPermissions={userPermissions} permission={openPermission}>
+        <PrimaryActionButton
+          disabled={!isReady(stack)}
+          onClick={() => openStack(stack)}
+          fullWidth
+        >
+          Open
+        </PrimaryActionButton>
+      </PermissionWrapper>}
+      {deleteStack && ownsNotebook && <PermissionWrapper className={classes.buttonWrapper} userPermissions={userPermissions} permission={deletePermission}>
+        <SecondaryActionButton
+          disabled={!isReady(stack)}
+          onClick={() => deleteStack(stack)}
+          fullWidth
+        >
+          Delete
+        </SecondaryActionButton>
+      </PermissionWrapper>}
+      {editStack && ownsNotebook && <PermissionWrapper className={classes.buttonWrapper} userPermissions={userPermissions} permission={editPermission}>
+        <SecondaryActionButton
+          disabled={!isReady(stack)}
+          onClick={() => editStack(stack)}
+          fullWidth
+        >
+          Edit
+        </SecondaryActionButton>
+      </PermissionWrapper>}
+    </div>
+  );
+};
 
 StackCardActions.propTypes = {
   stack: PropTypes.shape({
