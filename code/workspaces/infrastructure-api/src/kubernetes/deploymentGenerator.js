80e3a8f30c18d223c886c141135f465ba534ce28
iwalmsley <iain.walmsley@gmail.com>
2019-12-04 08:54:27 +0000

    Nercdl 528 merge jupyter lab and notebook code (#514)
    * feat: merge JupyterLab/Notebook to use common deployment code
    
    * fix: lint errors
    
    fix: remove empty line

--- code/workspaces/infrastructure-api/src/kubernetes/deploymentGenerator.js
+++ code/workspaces/infrastructure-api/src/kubernetes/deploymentGenerator.js
@@ -7,6 +7,7 @@ import config from '../config/config';
 const containerInfo = yaml.safeLoad(fs.readFileSync(config.get('containerInfoPath')));
 
 function createJupyterDeployment({ projectKey, deploymentName, notebookName, type, volumeMount }) {
+  const startCmd = type === 'jupyterlab' ? 'lab' : 'notebook';
   const context = {
     name: deploymentName,
     grantSudo: 'yes',
@@ -19,29 +20,11 @@ function createJupyterDeployment({ projectKey, deploymentName, notebookName, typ
     pySparkConfigMapName: nameGenerator.pySparkConfigMap(deploymentName),
     daskConfigMapName: nameGenerator.daskConfigMap(deploymentName),
     type,
+    startCmd,
     volumeMount,
   };
 
   return generateManifest(context, DeploymentTemplates.JUPYTER_DEPLOYMENT);
-}
-
-function createJupyterlabDeployment({ projectKey, deploymentName, notebookName, type, volumeMount }) {
-  const context = {
-    name: deploymentName,
-    grantSudo: 'yes',
-    domain: `${projectKey}-${notebookName}.${config.get('datalabDomain')}`,
-    jupyterlab: {
-      imageName: containerInfo.JUPYTERLAB_IMAGE,
-      version: containerInfo.JUPYTERLAB_VERSION,
-    },
-    serviceAccount: nameGenerator.computeSubmissionServiceAccount(projectKey),
-    pySparkConfigMapName: nameGenerator.pySparkConfigMap(deploymentName),
-    daskConfigMapName: nameGenerator.daskConfigMap(deploymentName),
-    type,
-    volumeMount,
-  };
-
-  return generateManifest(context, DeploymentTemplates.JUPYTERLAB_DEPLOYMENT);
 }
 
 function createZeppelinDeployment({ deploymentName, volumeMount, type }) {
@@ -133,11 +116,6 @@ function createJupyterService(notebookName) {
   return generateManifest(context, ServiceTemplates.JUPYTER_SERVICE);
 }
 
-function createJupyterlabService(notebookName) {
-  const context = { name: notebookName };
-  return generateManifest(context, ServiceTemplates.JUPYTERLAB_SERVICE);
-}
-
 function createZeppelinService(name) {
   const context = { name };
   return generateManifest(context, ServiceTemplates.ZEPPELIN_SERVICE);
@@ -193,14 +171,12 @@ function createDaskConfigMap(notebookName, projectKey) {
 
 export default {
   createJupyterDeployment,
-  createJupyterlabDeployment,
   createZeppelinDeployment,
   createRStudioDeployment,
   createRShinyDeployment,
   createNbViewerDeployment,
   createMinioDeployment,
   createJupyterService,
-  createJupyterlabService,
   createZeppelinService,
   createRStudioService,
   createRShinyService,
