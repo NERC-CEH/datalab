407d3e5b35e01dfd4b197076ef6dfa6212ed2c84
Iain <iainw@ceh.ac.uk>
2019-12-20 15:52:28 +0000

    feat: add project membership information
    fix: render correctly

--- code/workspaces/web-app/src/containers/projectInfo/ProjectInfoContainer.js
+++ code/workspaces/web-app/src/containers/projectInfo/ProjectInfoContainer.js
@@ -1,13 +1,20 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
 import { makeStyles } from '@material-ui/core/styles';
 import Typography from '@material-ui/core/Typography';
+import Chip from '@material-ui/core/Chip';
 import PromisedContentWrapper from '../../components/common/PromisedContentWrapper';
 import { useCurrentProject } from '../../hooks/currentProjectHooks';
+import { useProjectUsers } from '../../hooks/projectUsersHooks';
+import projectSettingsActions from '../../actions/projectSettingsActions';
 import ProjectKey from '../../components/common/typography/ProjectKey';
 
 const useStyles = makeStyles(theme => ({
   projectTitle: {
     marginRight: theme.spacing(2),
+  },
+  description: {
+    marginBottom: theme.spacing(2),
   },
   projectTitleWrapper: {
     display: 'flex',
@@ -22,10 +29,17 @@ const useStyles = makeStyles(theme => ({
     flexWrap: 'wrap',
     alignItems: 'baseline',
     marginTop: theme.spacing(2),
+    marginBottom: theme.spacing(2),
   },
   collaborationLinkHeading: {
     marginRight: theme.spacing(2),
     fontWeight: 'bold',
+  },
+  userBlock: {
+    paddingTop: theme.spacing(2),
+  },
+  chipTitle: {
+    textTransform: 'capitalize',
   },
 }));
 
@@ -35,8 +49,19 @@ const ProjectInfoContainer = () => {
 };
 
 export const PureProjectInfoContainer = ({ currentProject }) => {
-  const classes = useStyles();
   const { name, key, description, collaborationLink } = currentProject.value;
+  const classes = useStyles();
+  const users = useProjectUsers();
+  const dispatch = useDispatch();
+
+  useEffect(
+    () => {
+      if (key) {
+        dispatch(projectSettingsActions.getProjectUserPermissions(key));
+      }
+    },
+    [dispatch, key],
+  );
 
   return (
     <PromisedContentWrapper promise={currentProject}>
@@ -45,12 +70,23 @@ export const PureProjectInfoContainer = ({ currentProject }) => {
           <Typography variant="h5" className={classes.projectTitle}>{name}</Typography>
           <ProjectKey className={classes.projectKey}>{key && `(${key})`}</ProjectKey>
         </div>
-        <Typography variant="body1">{description}</Typography>
+        <Typography className={classes.description} variant="body1">{description}</Typography>
         <CollaborationLink link={collaborationLink}/>
+        <div className={classes.userBlock}>
+          <UserBlock name='admin' users={users.value.filter(user => user.role === 'admin')} classes={classes}/>
+          <UserBlock name='user' users={users.value.filter(user => user.role === 'user')} classes={classes}/>
+          <UserBlock name='viewer' users={users.value.filter(user => user.role === 'viewer')} classes={classes}/>
+        </div>
       </div>
     </PromisedContentWrapper>
   );
 };
+
+export const UserBlock = ({ name, users, classes }) => <div>
+    { users.length > 0
+      && <Typography className={classes.chipTitle} variant="h6">{name}s</Typography> }
+    { users.map(user => <Chip key={user.userId} tabIndex={-1} label={user.name}/>) }
+  </div>;
 
 export const CollaborationLink = ({ link }) => {
   const classes = useStyles();
