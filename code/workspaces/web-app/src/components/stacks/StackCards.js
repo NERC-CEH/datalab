1e48ebb05154c2ef8bffb8421974f2badf25a3c6
Alex Lee <alex.lee@tessella.com>
2020-09-11 10:15:30 +0100

    fix: remove unused imports (#573)

--- code/workspaces/web-app/src/components/stacks/StackCards.js
+++ code/workspaces/web-app/src/components/stacks/StackCards.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import Typography from '@material-ui/core/Typography';
-import { makeStyles, withStyles } from '@material-ui/core/styles';
+import { makeStyles } from '@material-ui/core/styles';
 import { sortBy } from 'lodash';
 import StackCard from './StackCard';
 import NewStackButton from './NewStackButton';
