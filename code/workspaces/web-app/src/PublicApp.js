732baaaed948e732160a9fc814af84f4ac91d533
Alex Lee <alex.lee@tessella.com>
2020-11-12 12:45:38 +0000

    fix: redirect to login page rather than give 404 when use not logged in

--- code/workspaces/web-app/src/PublicApp.js
+++ code/workspaces/web-app/src/PublicApp.js
@@ -3,15 +3,15 @@ import { Route, Switch } from 'react-router-dom';
 import { ThemeProvider as MuiThemeProvider } from '@material-ui/core/styles';
 import { publicAppTheme } from './theme';
 import WelcomePage from './pages/WelcomePage';
-import NotFoundPage from './pages/NotFoundPage';
 import VerifyEmailPage from './pages/VerifyEmailPage';
+import RedirectToLoginPage from './pages/RedirectToLoginPage';
 
 const PublicApp = () => (
   <MuiThemeProvider theme={publicAppTheme}>
     <Switch>
       <Route exact path="/" component={WelcomePage} />
       <Route exact path="/verify" component={VerifyEmailPage} />
-      <Route component={NotFoundPage} />
+      <Route component={RedirectToLoginPage} />
     </Switch>
   </MuiThemeProvider>
 );
