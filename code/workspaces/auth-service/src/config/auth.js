6ffd6355ac37422d0d9ffdc27bbdf0e968a71f0d
Andrew Longmore <andrew.longmore@tessella.com>
2020-11-17 13:04:03 +0000

    fix: undo change to auth.js

--- code/workspaces/auth-service/src/config/auth.js
+++ code/workspaces/auth-service/src/config/auth.js
@@ -5,6 +5,6 @@ export const PRIVATE_KEY = fs.readFileSync(config.get('privateKey'));
 export const PUBLIC_KEY = fs.readFileSync(config.get('publicKey'));
 export const algorithm = 'RS256';
 export const audience = config.get('authorisationAudience');
-export const expiresIn = '200m';
+export const expiresIn = '2m';
 export const issuer = config.get('authorisationIssuer');
 export const keyid = 'datalabs-authorisation';
