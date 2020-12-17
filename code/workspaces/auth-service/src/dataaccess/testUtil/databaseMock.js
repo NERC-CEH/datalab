8c4a994723b02f414164b802e2c2342dccce3844
Andrew Longmore <andrew.longmore@tessella.com>
2020-12-17 17:32:57 +0000

    fix: undo last commit, wrong branch

--- code/workspaces/auth-service/src/dataaccess/testUtil/databaseMock.js
+++ code/workspaces/auth-service/src/dataaccess/testUtil/databaseMock.js
@@ -1,15 +1,26 @@
+const cloneUser = user => ({
+  ...user,
+  projectRoles: [...user.projectRoles],
+});
 
-function createDatabaseMock(items) {
+const wrapUser = user => ({
+  ...user,
+  toObject: () => user,
+});
+
+function createDatabaseMock(users) {
+  const documents = users.map(cloneUser).map(wrapUser);
   let lastInvocation;
+  let findOneReturn;
 
   return () => ({
     find: (query) => {
       lastInvocation = { query };
-      return { exec: () => Promise.resolve(items) };
+      return { exec: () => Promise.resolve(documents) };
     },
     findOne: (query) => {
       lastInvocation = { query };
-      return { exec: () => Promise.resolve(items[0]) };
+      return { exec: () => Promise.resolve(findOneReturn) };
     },
     findOneAndUpdate: (query, entity, params) => {
       lastInvocation = { query, entity, params };
@@ -21,20 +32,18 @@ function createDatabaseMock(items) {
     },
     exists: (query) => {
       lastInvocation = { query };
-      return Promise.resolve(items.length > 0);
+      return Promise.resolve(documents.length > 0);
     },
     create: (entity) => {
-      const wrappedEntity = {
-        ...entity,
-        toObject: () => entity,
-      };
+      const document = wrapUser(cloneUser(entity));
       lastInvocation = { entity };
-      return Promise.resolve(wrappedEntity);
+      return Promise.resolve(document);
     },
     invocation: () => lastInvocation,
     query: () => lastInvocation.query,
     entity: () => lastInvocation.entity,
     params: () => lastInvocation.params,
+    setFindOneReturn: (user) => { findOneReturn = user ? wrapUser(cloneUser(user)) : undefined; },
     clear: () => { lastInvocation = undefined; },
   });
 }
