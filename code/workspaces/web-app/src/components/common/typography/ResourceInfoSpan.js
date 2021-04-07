e392236038e433c77872f1692501cc924cae2199
Andrew Longmore <andrew.longmore@tessella.com>
2021-04-07 17:06:27 +0100

    fix: improve styles in ResourceInfoSpan

--- code/workspaces/web-app/src/components/common/typography/ResourceInfoSpan.js
+++ code/workspaces/web-app/src/components/common/typography/ResourceInfoSpan.js
@@ -1,8 +1,7 @@
 import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
-import theme from '../../../theme';
 
-const useStyles = makeStyles(t => ({
+const useStyles = makeStyles(theme => ({
   root: {
     fontWeight: 200,
     letterSpacing: '0.05em',
