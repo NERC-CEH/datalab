3b22c683ed6be53369a31b81abb5c4d600ca1ca7
Andrew Longmore <andrew.longmore@tessella.com>
2021-05-18 17:54:42 +0100

    fix: remove unused parameters

--- code/workspaces/infrastructure-api/src/kubernetes/deploymentGenerator.js
+++ code/workspaces/infrastructure-api/src/kubernetes/deploymentGenerator.js
@@ -89,7 +89,7 @@ function createZeppelinDeployment({ deploymentName, volumeMount, type, version }
   return generateManifest(context, DeploymentTemplates.ZEPPELIN_DEPLOYMENT);
 }
 
-function createRStudioDeployment({ deploymentName, volumeMount, type, version, projectKey, name }) {
+function createRStudioDeployment({ deploymentName, volumeMount, type, version }) {
   const img = getImage(type, version);
   const context = {
     name: deploymentName,
