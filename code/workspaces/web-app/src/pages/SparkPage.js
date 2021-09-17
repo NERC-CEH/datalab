8399c6e2135967853dbba3541890b7f38028ad47
David Worley <david.worley@tessella.com>
2021-09-17 13:37:37 +0100

    Updated SparkPage to have ClustersContainer

--- code/workspaces/web-app/src/pages/SparkPage.js
+++ code/workspaces/web-app/src/pages/SparkPage.js
@@ -1,23 +1,32 @@
 import React from 'react';
 import Typography from '@material-ui/core/Typography';
+import { makeStyles } from '@material-ui/core/styles';
 import Page from './Page';
-import theme from '../theme';
-import PrimaryActionButton from '../components/common/buttons/PrimaryActionButton';
+import ClustersContainer from '../containers/clusters/ClustersContainer';
+import ExternalLink from '../components/common/ExternalLink';
+import { SPARK_CLUSTER_TYPE } from '../containers/clusters/clusterTypeName';
 
-const SparkPage = () => (
-  <Page title="Spark">
-    <Typography variant="body1">
-      Apache Spark is an open-source cluster-computing framework.<br/>
-      DataLabs supports usage of Spark via Kubernetes which allows Spark executors
-      to be deployed across the cluster.
-    </Typography>
-    <PrimaryActionButton
-      style={{ marginTop: theme.spacing(2) }}
-      onClick={() => window.open('https://spark.apache.org/docs/latest/running-on-kubernetes.html')}
-    >
-      Spark in DataLabs Documentation
-    </PrimaryActionButton>
-  </Page>
-);
+const useStyles = makeStyles(theme => ({
+  clusterList: {
+    marginTop: theme.spacing(5),
+  },
+}));
+
+const SparkPage = () => {
+  const classes = useStyles();
+
+  return (
+    <Page className={''} title="Spark">
+      <Typography variant="body1">
+      <ExternalLink href="https://spark.apache.org/">Apache Spark</ExternalLink> is an open-source cluster-computing framework.<br/>
+        DataLabs supports usage of Spark via Kubernetes which allows Spark executors
+        to be deployed across the cluster.
+      </Typography>
+      <div className={classes.clusterList}>
+        <ClustersContainer clusterType={SPARK_CLUSTER_TYPE} />
+      </div>
+    </Page>
+  );
+};
 
 export default SparkPage;
