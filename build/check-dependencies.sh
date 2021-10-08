68ea644df37c319b300fb9c9f2f6c020035e10bc
Ray <Millward>
2021-10-08 15:27:33 +0100

    feat: handled the yarn return code

--- build/check-dependencies.sh
+++ build/check-dependencies.sh
@@ -1,4 +1,19 @@
 #!/bin/bash
-set -e
-cd ./code && yarn audit --level=high --groups dependencies
 
+EXIT_INFO=1; 
+EXIT_LOW=2; 
+EXIT_MODERATE=4; 
+EXIT_HIGH=8; 
+EXIT_CRITICAL=16; 
+
+SEVERITY=$EXIT_HIGH
+
+(cd ./code && yarn audit --level=high --groups dependencies)
+t1=$?
+echo "Yarn audit result $t1"
+if [ $t1 -ge $SEVERITY ]; then
+  echo "Failed audit check";
+  exit 1;
+fi
+
+exit 0;
