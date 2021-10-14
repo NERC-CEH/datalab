13b865b8ee562c0ae561c92e524c4ad5bc0a50ef
Ray <Millward>
2021-10-14 14:01:11 +0100

    feat: updated sidebar to hide analysis if user is project viewer

--- code/workspaces/web-app/src/components/app/ProjectSideBar.js
+++ code/workspaces/web-app/src/components/app/ProjectSideBar.js
@@ -1,14 +1,18 @@
 import { withStyles } from '@material-ui/core/styles';
 import List from '@material-ui/core/List';
 import PropTypes from 'prop-types';
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
 import { permissionTypes } from 'common';
-import PermissionWrapper from '../common/ComponentPermissionWrapper';
+import { CurrentUserPermissionWrapper } from '../common/ComponentPermissionWrapper';
 import SideBarGroup from './SideBarGroup';
 import SideBarButton from './SideBarButton';
 import ProjectSwitcher from './ProjectSwitcher';
 import { useCurrentProjectKey } from '../../hooks/currentProjectHooks';
+import { useProjectUsers } from '../../hooks/projectUsersHooks';
+import { useCurrentUserId } from '../../hooks/authHooks';
 import sideBarStyles from './sideBarStyles';
+import projectSettingsActions from '../../actions/projectSettingsActions';
 
 const { projectPermissions: { PROJECT_KEY_STORAGE_LIST, PROJECT_KEY_STACKS_LIST, PROJECT_KEY_SETTINGS_LIST, PROJECT_KEY_CLUSTERS_LIST }, projectKeyPermission } = permissionTypes;
 
@@ -21,61 +25,67 @@ const InfoGroup = ({ projectKey }) => (
   </SideBarGroup>
 );
 
-const AnalysisGroup = ({ userPermissions, projectKey }) => (
+const AnalysisGroup = ({ projectKey }) => (
   <SideBarGroup title='Analysis'>
-    <PermissionWrapper userPermissions={userPermissions} permission={projectKeyPermission(PROJECT_KEY_STACKS_LIST, projectKey)}>
+    <CurrentUserPermissionWrapper permission={projectKeyPermission(PROJECT_KEY_STACKS_LIST, projectKey)}>
       <SideBarButton to={`${projectRouteBase(projectKey)}/notebooks`} label="Notebooks" icon="book" />
-    </PermissionWrapper>
-    <PermissionWrapper userPermissions={userPermissions} permission={projectKeyPermission(PROJECT_KEY_CLUSTERS_LIST, projectKey)}>
+    </CurrentUserPermissionWrapper>
+    <CurrentUserPermissionWrapper permission={projectKeyPermission(PROJECT_KEY_CLUSTERS_LIST, projectKey)}>
       <SideBarButton to={`${projectRouteBase(projectKey)}/dask`} label="Dask" icon="apps" />
-    </PermissionWrapper>
-    <SideBarButton to={`${projectRouteBase(projectKey)}/spark`} label="Spark" icon="apps" />
+    </CurrentUserPermissionWrapper>
+    <CurrentUserPermissionWrapper permission={projectKeyPermission(PROJECT_KEY_CLUSTERS_LIST, projectKey)}>
+      <SideBarButton to={`${projectRouteBase(projectKey)}/spark`} label="Spark" icon="apps" />
+    </CurrentUserPermissionWrapper>
   </SideBarGroup>
 );
 
-const MiscGroup = ({ userPermissions, projectKey }) => (
+const MiscGroup = ({ projectKey }) => (
   <SideBarGroup>
-    <PermissionWrapper userPermissions={userPermissions} permission={projectKeyPermission(PROJECT_KEY_STORAGE_LIST, projectKey)}>
+    <CurrentUserPermissionWrapper permission={projectKeyPermission(PROJECT_KEY_STORAGE_LIST, projectKey)}>
       <SideBarButton to={`${projectRouteBase(projectKey)}/storage`} label="Storage" icon="storage" />
-    </PermissionWrapper>
-    <PermissionWrapper userPermissions={userPermissions} permission={projectKeyPermission(PROJECT_KEY_STACKS_LIST, projectKey)}>
+    </CurrentUserPermissionWrapper>
+    <CurrentUserPermissionWrapper permission={projectKeyPermission(PROJECT_KEY_STACKS_LIST, projectKey)}>
       <SideBarButton to={`${projectRouteBase(projectKey)}/publishing`} label="Sites" icon="web" />
-    </PermissionWrapper>
-    <PermissionWrapper userPermissions={userPermissions} permission={projectKeyPermission(PROJECT_KEY_SETTINGS_LIST, projectKey)}>
+    </CurrentUserPermissionWrapper>
+    <CurrentUserPermissionWrapper permission={projectKeyPermission(PROJECT_KEY_SETTINGS_LIST, projectKey)}>
       <SideBarButton to={`${projectRouteBase(projectKey)}/settings`} label="Settings" icon="settings" />
-    </PermissionWrapper>
+    </CurrentUserPermissionWrapper>
   </SideBarGroup>
 );
 
-const ProjectSideBar = ({ classes, userPermissions }) => <PureSideBar
-  classes={classes}
-  userPermissions={userPermissions}
-  projectKey={useCurrentProjectKey()}
-/>;
+const ProjectSideBar = ({ classes, userPermissions }) => {
+  const projectKey = useCurrentProjectKey();
+  const currentUserId = useCurrentUserId();
+  const users = useProjectUsers();
+  const dispatch = useDispatch();
 
-export const PureSideBar = ({ classes, userPermissions, projectKey }) => (
-  <div className={classes.sideBar}>
+  useEffect(
+    () => {
+      if (projectKey.value) {
+        dispatch(projectSettingsActions.getProjectUserPermissions(projectKey.value));
+      }
+    },
+    [dispatch, projectKey],
+  );
+
+  const isLoading = users.fetching.inProgress || users.value.length === 0;
+  const isInstanceAdmin = userPermissions.findIndex(p => p === permissionTypes.SYSTEM_INSTANCE_ADMIN) > -1;
+
+  const isViewer = !isInstanceAdmin
+                   && (isLoading || (users && users.value && users.value.findIndex(u => u.userId === currentUserId && u.role === 'viewer') > -1));
+
+  return (<div className={classes.sideBar}>
     <List className={classes.itemList}>
       <InfoGroup classes={classes} projectKey={projectKey.value} />
-      <AnalysisGroup userPermissions={userPermissions} projectKey={projectKey.value} />
-      <MiscGroup userPermissions={userPermissions} projectKey={projectKey.value} />
+      {!isLoading && !isViewer && <AnalysisGroup projectKey={projectKey.value} />}
+      {!isLoading && <MiscGroup projectKey={projectKey.value} />}
     </List>
-  </div>
-);
+  </div>);
+};
 
 ProjectSideBar.propTypes = {
   classes: PropTypes.object.isRequired,
   userPermissions: PropTypes.arrayOf(PropTypes.string).isRequired,
-};
-
-PureSideBar.propTypes = {
-  classes: PropTypes.object.isRequired,
-  userPermissions: PropTypes.arrayOf(PropTypes.string).isRequired,
-  projectKey: PropTypes.shape({
-    fetching: PropTypes.bool.isRequired,
-    error: PropTypes.oneOfType([PropTypes.string, PropTypes.object]),
-    value: PropTypes.string,
-  }),
 };
 
 export default withStyles(sideBarStyles)(ProjectSideBar);
