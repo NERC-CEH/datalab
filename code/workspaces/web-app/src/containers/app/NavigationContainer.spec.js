b68a31c8005cbd21e75ddce1bd102715b904512d
David Worley <david.worley@tessella.com>
2021-11-26 15:56:13 +0000

    Updated some of the app navigation tests to use react testing library

--- code/workspaces/web-app/src/containers/app/NavigationContainer.spec.js
+++ code/workspaces/web-app/src/containers/app/NavigationContainer.spec.js
@@ -1,48 +1,18 @@
 import React from 'react';
-import { shallow } from 'enzyme';
-import createStore from 'redux-mock-store';
-import NavigationContainer, { PureNavigationContainer } from './NavigationContainer';
+import { render } from '@testing-library/react';
+import { PureNavigationContainer } from './NavigationContainer';
 
-jest.mock('../../components/app/Navigation', () => () => (<>Navigation</>));
+jest.mock('../../components/app/Navigation', () => props => (<>Navigation Mock {JSON.stringify(props)}</>));
 
 describe('NavigationContainer', () => {
-  describe('is a connected component which', () => {
-    function shallowRenderConnected() {
-      const store = createStore()({
-        authentication: {
-          identity: { expected: 'Identity' },
-        },
-      });
-
-      const props = {
-        store,
-        PrivateComponent: () => {},
-        PublicComponent: () => {},
-      };
-
-      return shallow(<NavigationContainer {...props} />).find('NavigationContainer');
-    }
-
-    it('extracts the correct props from the redux state', () => {
-      // Act
-      const output = shallowRenderConnected();
-
-      // Assert
-      expect(output.prop('identity')).toEqual({ expected: 'Identity' });
-    });
-  });
-
   describe('is a container which', () => {
-    function shallowRenderPure() {
-      const props = {
-        identity: { expected: 'Identity' },
-        anotherProp: 'something',
-      };
+    const props = {
+      identity: { expected: 'Identity' },
+      anotherProp: 'something',
+    };
 
-      return shallow(<PureNavigationContainer {...props} />);
-    }
     it('passes correct props to Navigation', () => {
-      expect(shallowRenderPure()).toMatchSnapshot();
+      expect(render(<PureNavigationContainer {...props} />).container).toMatchSnapshot();
     });
   });
 });
